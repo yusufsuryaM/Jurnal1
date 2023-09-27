1644a549bdaeceec37063d6be2bed9bad6908509
yusufsuryamulyawan <yusufsuryamulyawan@gmail.com>
2023-09-27 19:28:10 +0700

    Initial commit

---
+++ Jurnal1.java
@@ -0,0 +1,12 @@
+public class Jurnal1 {
+    public static void main(String[] args) {
+        String nama = "Yusuf Surya Mulyawan";
+        String nim = "607062300080";
+        String kelas = "D3IF-47-3";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
+
